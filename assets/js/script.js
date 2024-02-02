fda407c21f257908a50ccb0a24a1c712fedbd0fe
Zehra Ecer <zehraecer1996@hotmail.com>
2024-02-02 17:59:28 +0300

    js denemeleri yapıldı

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".btp")
+
+
+
+function getPage(){
+    btn.addEventListener("click",function(){
+        
+        const body = document.querySelector(".bla")
+        body.innerHTML=`dlsgmklfhmdk
+`
+        console.log(btn);
+    })
+}
+getPage();
